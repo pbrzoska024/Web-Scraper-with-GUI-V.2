bcef293ac322f89d01600a354f5ab3743055dd9f
Paweł Brzóska <53401206+pbrzoska024@users.noreply.github.com>
2021-05-27 13:28:15 +0200

    Update SCRAPPERMAIN.py

--- SCRAPPERMAIN.py
+++ SCRAPPERMAIN.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import csv
 import collections
 import requests
@@ -8,6 +9,9 @@ from requests.exceptions import RequestException
 from bs4 import BeautifulSoup
 from tkinter import *
 from tkinter import messagebox
+from matplotlib.pyplot import pie, axis, show
+
+
 
 
 # Main scrapper frame class initialization
@@ -31,8 +35,8 @@ scrollbar = Scrollbar(myapp)
 scrollbar.pack(side=RIGHT, fill=Y)
 
 
-# Text scrap definition
-def urlscrap():
+# Main scrap function and putting it all togheter in widnows
+def urlscraptowindow():
  v = e.get()
 
  if v == "":
@@ -49,8 +53,8 @@ def urlscrap():
      f.config(state=DISABLED)
 
 
-# Function for scraping href links
-def opening():
+# Main function for scraping links from sites
+def scraplinksandsave():
  v = e.get()
 
  if v == "":
@@ -68,8 +72,8 @@ def opening():
       tk.messagebox.showinfo("Succes", "Scrapped all lInks from the site")
 
 
-
-def opening2():
+#Scrap text and save
+def scraptextandsave():
  v = e.get()
 
  if v == "":
@@ -86,7 +90,7 @@ def opening2():
 
       tk.messagebox.showinfo("Succes", "Scrapped whole text from  the site")
 
-def graphdraw():
+def splitandcount():
  letters = set("AEIOUaeioubcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ")
  vowels = set("AEIOUaeiou")
  cons = set("bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ")
@@ -112,31 +116,41 @@ def graphdraw():
 
  tk.messagebox.showinfo("SUCCES")
 
+#Analyze
+def analyze():
+     df = pd.read_csv ('log.csv')
+
+     vowels_data = df["Vowels"]
+     cons_data = df["Cons"]
+     colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#8c564b"]
+     explode = (0.1, 0, 0, 0, 0)
+     plt.pie(Vowels_data, Cons_data, explode=explode, colors=colors,
+     autopct='%1.1f%%', shadow=True, startangle=140)
+     plt.title("Vowels and cons pie chart")
+     plt.show()
+
+
 
 #Scrapper button 0.9
-f = Button(myapp, text="Scrap and show text from site", command=urlscrap)
+f = Button(myapp, text="Scrap and show text from site", command=urlscraptowindow)
 f.pack()
 
 #Scrapper button 1
-b = Button(myapp, text="Scrap Href Links from site an save as TXT", command=opening)
+b = Button(myapp, text="Scrap Href Links from site an save as TXT", command=scraplinksandsave)
 b.pack()
 
 
 #BUTTON:Scrap text and save as TXT
-c = Button(myapp, text="Scrap text from site an save as TXT", command=opening2)
+c = Button(myapp, text="Scrap text from site an save as TXT", command=scraptextandsave)
 c.pack()
 
 #BUTTON:Count Vovels and Cons
-h = Button(myapp, text="Count Vovels and Cons from TXT and save as CSV", command=graphdraw)
+h = Button(myapp, text="Count Vovels and Cons from TXT and save as CSV", command=splitandcount)
 h.pack()
 
+#BUTTON:Analyze vovels and visualize
+j = Button(myapp, text="Analyze Vovels and Cons", command=analyze)
+j.pack()
 
 #start the program
 myapp.mainloop()
-
-
-
-
-
-
-
